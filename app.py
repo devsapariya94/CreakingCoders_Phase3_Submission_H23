49d081b099d574c94cc3218c511ad08d660a35ce
Dev <devsapariya94@gmail.com>
2023-08-27 03:46:34 +0530

    home page setup

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
-from flask import Flask, request, jsonify, render_template,flash
+from flask import Flask, request, jsonify, render_template,flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, logout_user, login_required, current_user, LoginManager,UserMixin
 import os
+from oauthlib.oauth2 import WebApplicationClient
+import json
+import requests
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+session = {}
 secret_key = os.urandom(24)
 app.config['SECRET_KEY'] = secret_key
+
+with open('config.json', 'r') as f:
+    params = json.load(f)["params"]
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+GOOGLE_CLIENT_ID = params["google_client_id"]
+# GOOGLE_CLIENT_SECRET = params["google_client_secret"]
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
 @login_manager.user_loader
 
+
+#for the locally run server outh2 need https but lo
 
 def get_user(ident):
   return User.query.get(int(ident))
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50))
     email = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(50))
     role = db.Column(db.Integer)
+    auth_type = db.Column(db.String(50))
+
     def __repr__(self):
         return '<User %r>' % self.name
 
@@ -53,26 +75,27 @@ if not os.path.exists('database.db'):
 
 @app.route('/')
 def index():
+    session['url']="/"
+    if current_user.is_authenticated:
+        return render_template('home.html',user=current_user)
+    
     return render_template('index.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    session['url']="/login"
     if current_user.is_authenticated:
-        return render_template('home.html',user=current_user)
+        return redirect("/")
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        print(username, password)
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(email=email, password=password).first()
         if user:
-            print(user.password)
-            print(type(password))
-            print(type(user.password))
             if user.password == password:
                 print('User authenticated')
                 login_user(user)
-                return render_template('home.html',user=current_user)
+                return redirect("/")
             else:
                 return render_template('login.html', error='Incorrect password')
         else:
@@ -89,17 +112,19 @@ def logout():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    session['url']="/register"
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        auth_type = "local"
         if User.query.filter_by(username=username).first():
             return render_template('register.html', error='User already exists')
         
         if User.query.filter_by(email=email).first():
             return render_template('register.html', error='Email already exists')
         print(username, password, email)
-        user = User(username=username, password=password, email=email, role=3)
+        user = User(username=username, password=password, email=email, role=3, auth_type=auth_type)
         db.session.add(user)
         db.session.commit()
         print('User created')
@@ -141,11 +166,35 @@ def patient():
 
 
 
+@app.route('/add_admin', methods=['POST'])
+def add_admin():
+    username = "admin"
+    password = "admin"
+    email = "admin@xyz.com"
+    role = 1
+    auth_type = "local"
+    user = User(username=username, password=password, email=email, role=role, auth_type=auth_type)
+    db.session.add(user)
+    db.session.commit()
+    return render_template('index.html')
+
+
+#book apoitment
+#healt record history
+#doctor shcedule
+#manage doctor
+    #doctor list
+#manage pharmacy
+#view all patient
+#all doctor schedual
+#create summery of all doctor that how many patient they have seen at the end of the day to admin
+#meeting with admin 
         
 
 #add prescription by the doctor
 @app.route('/add_prescription', methods=['POST'])
 def add_prescription():
+
     if current_user.role != 2:
         return render_template('index.html')
     patient_id = request.form['patient_id']
@@ -207,6 +256,103 @@ def get_patient():
         suggestions = [patient.name for patient in patients]
 
     return render_template('suggestions.html', suggestions=suggestions)
+
+
+
+
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@app.route("/login/google")
+def google_login():
+    google_provider_cfg = get_google_provider_cfg()
+    auth_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    request_uri = client.prepare_request_uri(
+        auth_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+
+    )
+    
+
+    return redirect(request_uri)
+
+@app.route("/login/google/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url="http://127.0.0.1:5000/login/google/callback",
+        code=code,
+    )
+
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(params["google_client_id"], params["google_client_secret"]),
+    )
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    userinfo_data = userinfo_response.json()
+
+    if userinfo_response.json().get("email_verified"):
+        users_email = userinfo_data["email"]
+        user = User.query.filter_by(email=users_email).first()
+
+        if user:
+            if user.auth_type == "google":
+                login_user(user)
+                return redirect(session['url'])
+            else:
+                return render_template('home.html', user=current_user)
+        else:
+            # Create a new user using Google account information
+            username = userinfo_data.get("name", "Google User")
+            auth_type = "google"
+            role = 3
+            user = User(username=username, email=users_email, auth_type=auth_type, role=role)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect(session['url'])
+    else:
+        return "User email not available or not verified by Google.", 400
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/test')
+def test():
+    return render_template('userdash.html')
+
+
+
+
+
+
+
+
 
 
 
